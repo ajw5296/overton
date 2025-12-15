94f274910d4a7f90739db925e5585ba65494d9d6
Alex Winters <awin117@gmail.com>
2025-12-15 16:17:00 -0500

    restart

--- metrics/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Overton Metrics Package
-Plugin-based metric extraction system for Overton API data.
-"""
-
-from metrics.base_extractor import MetricExtractor
-from metrics.registry import registry
-
-__all__ = ['MetricExtractor', 'registry']
